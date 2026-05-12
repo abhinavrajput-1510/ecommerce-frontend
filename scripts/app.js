b2e05c69bf1ad5513877048133fdfbc871dcffc4
Abhinav <abhinavverma1699@gmail.com>
2026-05-12 19:49:20 +0530

    completed the nav section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mainNav = document.querySelector(".main-nav");
+
+if (hamburgerMenu && mainNav) {
+  hamburgerMenu.addEventListener("click", () => {
+    mainNav.classList.toggle("open");
+    const expanded = mainNav.classList.contains("open");
+    hamburgerMenu.setAttribute("aria-expanded", expanded);
+  });
+}
+
 console.log("E-Commerce Website Loaded");
